9b106a75445d529809622f8bbc1cab3535053672
Acb897 <camara.alber@gmail.com>
2024-11-20 12:51:40 +0100

    Fixed triple pattern from query extractor to work with optional clauses

--- V2/test.rb
+++ V2/test.rb
@@ -1,6 +1,6 @@
 require_relative "./Transform.rb"
 require "SPARQL"
-
+require "uri"
 # @sparql = "SELECT ?person ?city WHERE { OPTIONAL {?person a dbpedia-owl:Architect} ?person dbpprop:birthPlace ?city. ?city dc:subject dbpedia:Category:Capitals_in_Europe. } LIMIT 100"
 # @sparql = "SELECT ?person ?city WHERE  {?person a dbpedia-owl:Architect . ?person dbpprop:birthPlace ?city. ?city dc:subject dbpedia:Category:Capitals_in_Europe. } LIMIT 100"
 
@@ -11,7 +11,7 @@ PREFIX dc: <http://purl.org/dc/terms/>
 PREFIX dbpedia: <http://dbpedia.org/resource/Category:> 
 SELECT ?person ?city WHERE  {
 ?person a dbpedia-owl:Architect . 
-?person dbpprop:birthPlace ?city. 
+OPTIONAL {?person dbpprop:birthPlace ?city}. 
 ?city dc:subject dbpedia:Capitals_in_Europe. 
 } LIMIT 100"
 
@@ -26,6 +26,18 @@ SELECT ?person ?city WHERE  {
 #     ?person vcard:FN  ?name .
 #     OPTIONAL { ?person info:age ?age }
 # }"
-transform(@sparql)
 
-print @bgp
+
+testing = transform(@sparql)
+
+puts testing
+
+
+# def decode_url(encoded_url)
+#   URI.decode_www_form_component(encoded_url)
+# end
+
+# encoded_url = "https://fragments.dbpedia.org/2016-04/en?subject=&amp;predicate=rdf%3Atype&amp;object=http%3A%2F%2Fdbpedia.org%2Fontology%2FArchitect&amp;page=2"
+# decoded_url = decode_url(encoded_url)
+
+# puts decoded_url
